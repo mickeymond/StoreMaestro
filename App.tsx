2c4f137414e157c3adbb4bec3ddb99550dca3815
mickeymond <33749130+mickeymond@users.noreply.github.com>
2025-01-04 01:56:07 +0000

    installing the native stack navigator

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@
  * @format
  */
 
+import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { View } from 'react-native';
 import { Appbar, PaperProvider, Text, TouchableRipple } from 'react-native-paper';
@@ -13,20 +14,22 @@ import { Appbar, PaperProvider, Text, TouchableRipple } from 'react-native-paper
 function App(): React.JSX.Element {
   return (
     <PaperProvider>
-      <View>
-        <Appbar.Header>
-          <Appbar.BackAction onPress={() => { }} />
-          <Appbar.Content title="Title" />
-          <Appbar.Action icon="calendar" onPress={() => { }} />
-          <Appbar.Action icon="magnify" onPress={() => { }} />
-        </Appbar.Header>
-        <TouchableRipple
-          onPress={() => console.log('Pressed')}
-          rippleColor="rgba(0, 0, 0, .32)"
-        >
-          <Text>Press anywhere</Text>
-        </TouchableRipple>
-      </View>
+      <NavigationContainer>
+        <View>
+          <Appbar.Header>
+            <Appbar.BackAction onPress={() => { }} />
+            <Appbar.Content title="Title" />
+            <Appbar.Action icon="calendar" onPress={() => { }} />
+            <Appbar.Action icon="magnify" onPress={() => { }} />
+          </Appbar.Header>
+          <TouchableRipple
+            onPress={() => console.log('Pressed')}
+            rippleColor="rgba(0, 0, 0, .32)"
+          >
+            <Text>Press anywhere</Text>
+          </TouchableRipple>
+        </View>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
