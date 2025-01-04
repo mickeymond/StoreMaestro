efd99cefe63ebea5a29bab4576434fdeb7949526
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2025-01-04 09:23:55 +0000

    UI/add product (#5)
    * wiring up the add product component
    
    * installing @react-native-firebase/firestore
    
    * add product looking good

---
+++ src/components/AddProduct.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Button, Card, TextInput } from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+import { PRODUCTS_COLLECTION } from '../core/constants';
+
+export function AddProduct({ dismissModal }: {
+  dismissModal: Function
+}) {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
+  const submit = () => {
+    // console.log('Submitting product!');
+    firestore()
+      .collection(PRODUCTS_COLLECTION)
+      .add({ name, price })
+      .then(() => {
+        // console.log('Product added!');
+        dismissModal();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <Card>
+      <Card.Title
+        titleStyle={{ fontWeight: "bold", textAlign: "center", fontSize: 18, marginTop: 30 }}
+        title="Add New Product" />
+      <Card.Content>
+        <TextInput
+          style={{ marginVertical: 15 }}
+          label="Product Name"
+          mode="outlined"
+          value={name}
+          onChangeText={text => setName(text)}
+        />
+        <TextInput
+          style={{ marginVertical: 15 }}
+          label="Product Price"
+          mode="outlined"
+          value={price}
+          onChangeText={text => setPrice(text)}
+        />
+      </Card.Content>
+      <Card.Actions>
+        <Button onPress={() => dismissModal()}>Cancel</Button>
+        <Button onPress={submit} disabled={!name || !price}>Submit</Button>
+      </Card.Actions>
+    </Card>
+  );
+}
--- src/screens/Products.tsx
+++ src/screens/Products.tsx
@@ -1,23 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
-import { Avatar, Button, Card, FAB, Searchbar, Surface } from 'react-native-paper';
+import { Avatar, Button, Card, FAB, Modal, Searchbar, Surface } from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+import { AddProduct } from '../components';
+import { PRODUCTS_COLLECTION } from '../core/constants';
+import { useDebounce } from 'use-debounce';
 
 const PRODUCTS = [
-  { id: '1', title: 'Bel Aqua Medium (750ml)', price: 30.00 },
-  { id: '2', title: 'Bel Aqua Small (500ml)', price: 25.00 },
-  { id: '3', title: 'Awake Medium (750ml)', price: 28.00 },
-  { id: '4', title: 'Awake Small (500ml)', price: 25.00 },
-  { id: '5', title: 'Awake Small (500ml)', price: 25.00 },
-  { id: '6', title: 'Awake Small (500ml)', price: 25.00 },
-  { id: '7', title: 'Awake Small (500ml)', price: 25.00 },
+  { id: '1', name: 'Bel Aqua Medium (750ml)', price: 30.00 },
+  { id: '2', name: 'Bel Aqua Small (500ml)', price: 25.00 },
+  { id: '3', name: 'Awake Medium (750ml)', price: 28.00 },
+  { id: '4', name: 'Awake Small (500ml)', price: 25.00 },
+  { id: '5', name: 'Awake Small (500ml)', price: 25.00 },
+  { id: '6', name: 'Awake Small (500ml)', price: 25.00 },
+  { id: '7', name: 'Awake Small (500ml)', price: 25.00 },
 ];
 
+type Product = {
+  id: string;
+  name: string;
+  price: string;
+}
+
 export function ProductsScreen() {
+  const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState('');
+  const [debouncedQuery] = useDebounce(query, 1000);
+  const [addingProduct, setAddingProduct] = useState(false);
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection(PRODUCTS_COLLECTION)
+      .where('name', '>=', debouncedQuery)
+      .where('name', '<=', debouncedQuery + '~')
+      .onSnapshot(documentSnapshot => {
+        const products = documentSnapshot.docs.map(doc => {
+          return { id: doc.id, name: doc.data().name, price: doc.data().price }
+        });
+        setProducts(products);
+      });
+
+    // Stop listening for updates when no longer required
+    return () => subscriber();
+  }, [debouncedQuery]);
 
   return (
     <Surface
-      style={{ marginBottom: 150 }}
+      style={{ marginBottom: 150, minHeight: '100%' }}
       elevation={0}>
       <Searchbar
         style={{ margin: 10 }}
@@ -26,13 +55,16 @@ export function ProductsScreen() {
         value={query}
       />
       <FlatList
-        data={PRODUCTS}
+        data={products}
         renderItem={({ item }) => (
           <Card style={{ margin: 10 }}>
             <Card.Title
-              title={item.title}
+              title={item.name}
               subtitle={`GHS ${item.price}`}
-              left={(props) => <Avatar.Image {...props} source={{ uri: 'https://picsum.photos/700' }} />}
+              left={(props) => <Avatar.Image
+                {...props}
+                source={{ uri: `https://avatar.iran.liara.run/username?username=${item.name}` }}
+              />}
             />
             <Card.Actions>
               <Button>Edit</Button>
@@ -45,8 +77,15 @@ export function ProductsScreen() {
       <FAB
         icon="plus"
         style={{ position: 'absolute', bottom: 10, right: 10 }}
-        onPress={() => console.log('Pressed')}
+        onPress={() => setAddingProduct(true)}
       />
+      <Modal
+        visible={addingProduct}
+        dismissable={false}
+        onDismiss={() => setAddingProduct(false)}
+        contentContainerStyle={{ backgroundColor: 'white', marginHorizontal: 10, borderRadius: 10 }}>
+        <AddProduct dismissModal={() => setAddingProduct(false)} />
+      </Modal>
     </Surface>
   );
 }
